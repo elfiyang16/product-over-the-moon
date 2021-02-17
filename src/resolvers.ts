aa081e7187a887f49fe02bf2096f578cb656779d
Elfi Yang <elfiyang16@gmail.com>
2021-02-17 20:21:18 +0000

    feat: add get all & get by id

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,67 @@
-const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
-
+const { unmarshall, marshall } = require('@aws-sdk/util-dynamodb');
+const {
+  DynamoDBClient,
+  ScanCommand,
+  GetItemCommand,
+  QueryCommand,
+  PutItemCommand,
+} = require('@aws-sdk/client-dynamodb');
+const { uuid } = require('uuidv4');
 const client = new DynamoDBClient({ region: 'eu-west-1' });
 
 const TABLENAME = 'ProductInfo';
 
-export const resolvers = {};
+// Get Product By Id
+const getProductById = async (id) => {
+  const params = {
+    TableName: TABLENAME,
+    Key: marshall({
+      id: id,
+    }),
+  };
+
+  try {
+    const results = await client.send(new GetItemCommand(params));
+    let product: any;
+    product = unmarshall(results.Item);
+    return product;
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+};
+
+// Get Products (scan)
+
+const getProducts = async () => {
+  const params = {
+    TableName: TABLENAME,
+  };
+
+  try {
+    const results = await client.send(new ScanCommand(params));
+    const products = [];
+    results.Items.forEach((item) => {
+      // convert the DynamoDB JSON format to plain JSON
+      //@ts-ignore
+      products.push(unmarshall(item));
+    });
+    return products;
+  } catch (err) {
+    console.error(err);
+    return err;
+  }
+};
+
+// ====================================
+
+export const resolvers = {
+  Query: {
+    products: () => {
+      return getProducts();
+    },
+    product: (root, args) => {
+      return getProductById(args.id);
+    },
+  },
+};
