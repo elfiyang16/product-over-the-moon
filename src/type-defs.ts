126ad3a247cde71758346e0657f93700f93119ce
Elfi Yang <elfiyang16@gmail.com>
2021-02-17 20:09:18 +0000

    feat: add typedefs

--- src/type-defs.ts
+++ src/type-defs.ts
@@ -1,3 +1,26 @@
 import { gql } from 'apollo-server-lambda';
 
-export const typeDefs = gql``;
+export const typeDefs = gql`
+  scalar JSON
+  type Product {
+    id: ID!
+    name: String
+    quantity: String
+    vendor: String
+    vendor_email: String
+  }
+  type Query {
+    products: [Product]
+    product(id: ID!): Product
+    productByName(name: String!): Product
+  }
+  type Mutation {
+    addProduct(input: AddProductInput!): Product
+  }
+  input AddProductInput {
+    name: String!
+    quantity: String!
+    vendor: String!
+    vendor_email: String!
+  }
+`;
